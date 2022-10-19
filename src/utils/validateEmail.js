dc2a6132aaf42c9a5c70a9df1ca7810663ee8a69
Ashutosh Thakur <ashutoshthakur_14@yahoo.com>
2022-10-19 17:11:39 +0530

    Added check domain feature with unit tests

--- src/utils/validateEmail.js
+++ src/utils/validateEmail.js
@@ -1,4 +1,17 @@
-function validateEmail(email) {
+function validateEmail(email,domain = " ") {
+
+    if(email === null || email === undefined) {
+        return false;
+    }
+    if(domain != " ") {
+        let emailSplit = email.split("@")
+        let emailDomain = emailSplit[1];
+
+        if(emailDomain !== domain)
+        {
+            return false;
+        }
+    }
     // RFC 5322 Format regex
     let regex = new RegExp("([!#-'*+/-9=?A-Z^-~-]+(\.[!#-'*+/-9=?A-Z^-~-]+)*|\"\(\[\]!#-[^-~ \t]|(\\[\t -~]))+\")@([!#-'*+/-9=?A-Z^-~-]+(\.[!#-'*+/-9=?A-Z^-~-]+)*|\[[\t -Z^-~]*])");
     return regex.test(email)
--- test/validators.test.js
+++ test/validators.test.js
@@ -4,22 +4,45 @@ describe('Email Validation Tests', () => {
 
     it("validating correct email", () => {
         let testEmail1 = "ashutoshthakur1409@gmail.com"
-        console.log(validInfo.version)
         let resultData = validInfo.validateEmail(testEmail1);
         expect(resultData).toBe(true);
     })
 
     it("validating incorrect email", () => {
         let testEmail2 = "@ashutoshthakur1409gmail.123"
-        console.log(validInfo.version)
         let resultData = validInfo.validateEmail(testEmail2);
         expect(resultData).toBe(false);
     })
 
     it("validating incorrect email", () => {
         let testEmail3 = `"  abcd egh lmonp   "@example.com`
-        console.log(validInfo.version)
         let resultData = validInfo.validateEmail(testEmail3);
         expect(resultData).toBe(false);
+    })
+
+    it("checking null case", () => {
+        let testEmail4 = null;
+
+        let resultData = validInfo.validateEmail(testEmail4);
+
+        expect(resultData).toBe(false);
+    })
+
+    it("checking email domain", () => {
+        let testEmail5 = "ashutoshthakur1409@gmail.com";
+        let testDomain = "gmail.com";
+
+        let resultData = validInfo.validateEmail(testEmail5,testDomain);
+
+        expect(resultData).toBe(true)
+    })
+
+    it("checking email domain not match", () => {
+        let testEmail5 = "ashutoshthakur1409@gmail.com";
+        let testDomain = "yahoo.com";
+
+        let resultData = validInfo.validateEmail(testEmail5,testDomain);
+
+        expect(resultData).toBe(false)
     })
  })
